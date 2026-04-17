36740170aeb64820e03b350f21cabbb0c0959a29
gopendrasharma89-tech <gopendrasharma89@gmail.com>
2026-04-17 16:59:29 +0530

    Delete settings.gradle.kts

--- settings.gradle.kts
+++
@@ -1,4 +0,0 @@
-pluginManagement { repositories { google(); mavenCentral() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name = "OpenClawGo"
-include(":app")
